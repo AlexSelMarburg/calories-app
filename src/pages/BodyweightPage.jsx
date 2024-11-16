6b22b30871a0b43d712f8aa7bb5795f166206abe
AlexSelMarburg <Selalx2@aol.com>
2024-11-16 13:45:03 +0100

    storage test

--- src/pages/BodyweightPage.jsx
+++ src/pages/BodyweightPage.jsx
@@ -1,7 +1,12 @@
 export default function BodyweightPage() {
+  const storage = localStorage.getItem("calories");
+
+  console.log(storage);
+
   return (
     <div className="page">
       <h2>BodyweightPage</h2>
+      <p>{storage}</p>
     </div>
   );
 }
